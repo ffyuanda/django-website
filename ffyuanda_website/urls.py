5e9d5b859049abc2f2bc73eb9df7e7658b3859b6
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-06-19 14:11:00 +0800

    feat: routes reorganized
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

--- ffyuanda_website/urls.py
+++ ffyuanda_website/urls.py
@@ -14,17 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
-
-    # Django Auth
-    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path("admin/", admin.site.urls),
+    path("", include('ffyuanda_website.apps.public.urls')),
+    path("accounts/", include('ffyuanda_website.apps.accounts.urls')),
 ]
