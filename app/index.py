b65f4b427e02b79fc32eec4c293804df305370ee
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-06-29 14:56:19 +0800

    feat: favicon added
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

--- app/index.py
+++ app/index.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    print()
-    url = url_for('static', filename='style.css')
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -19,5 +17,4 @@ def show_post(post_id):
 @app.route('/login', methods=['POST', 'GET'])
 @app.route('/login/<name>')
 def login(name=None):
-    # if request.method == 'POST':
     return render_template('login.html', name=name)
